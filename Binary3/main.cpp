aa74a86f0f7d54c5ffa4c4294e924979dfd31dc1
Nikolay Elshin <nikolay19830223@gmail.com>
2023-08-31 03:51:21 +0800

    EConstructor, EDistructor is done

--- Binary3/main.cpp
+++ Binary3/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
+class Tree
+{
+	class Element
+	{
+		int Data;
+		Element* pLeft;
+		Element* pRight;
+	public:
+		Element(int Data, Element* pLeft = nullptr, Element* pRight = nullptr) :Data(Data), pLeft(pLeft), pRight(pRight)
+		{
+			std::cout << "EConstructor:\t" << this << std::endl;
+		}
+		~Element()
+		{
+			std::cout << "EDestructor:\t" << this << std::endl;
+		}
+	} *Root;
+};
 
 void main()
 {
