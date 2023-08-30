139bb4dd3ca7cf317b39711c36326c44afc1a7f9
Nikolay Elshin <nikolay19830223@gmail.com>
2023-08-31 06:22:05 +0800

    Factorial funcion is done

--- recursion/main.cpp
+++ recursion/main.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 
 void elevator(int floor);
+int Factorial(int n);
 
 //#define ELEVATOR_CHEK
+//#define FACTORIAL_CHEK
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -11,6 +13,11 @@ void main()
 	std::cout << "Введите номер этажа: "; std::cin >> n;
 	elevator(n);
 #endif // ELEVATOR_CHEK
+#ifdef FACTORIAL_CHEK
+	int n;
+	std::cout << "Введите число: "; std::cin >> n;
+	(n < 0) ? std::cout << "Вы ввели отрицательное число!" : std::cout << "Факториал числа " << n << " будет: " << Factorial(n);
+#endif // FACTORIAL_CHEK
 
 
 }
@@ -24,4 +31,9 @@ void elevator(int floor)
 	std::cout << "Вы на " << floor << " этаже\n";
 	elevator(floor > 0?floor - 1: floor + 1);
 	std::cout << "Вы на " << floor << " этаже\n";
+}
+int Factorial(int n)
+{
+	if (n == 0) return 1;
+	return n * Factorial(n - 1);
 }
