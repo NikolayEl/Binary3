93db81dcf6013a2355ea153b3851d6b9d6f961b5
Nikolay Elshin <nikolay19830223@gmail.com>
2023-09-02 23:46:59 +0800

    no Root whith funcions, Destructor - is done

--- Binary3/main.cpp
+++ Binary3/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define delimitr "\n------------------------------------------------------\n"
 
 class Tree
 {
@@ -15,13 +16,14 @@ protected:
 		}
 		~Element()
 		{
+			
 			std::cout << "EDestructor:\t" << this << std::endl;
 		}
 		friend class Tree;
 		friend class UniqueTree;
 	} *Root;
 public:
-	Element* gerRoot()
+	Element* getRoot()
 	{
 		return Root;
 	}
@@ -31,10 +33,29 @@ public:
 	}
 	~Tree()
 	{
+		Destructor();
+		delete Root;
 		std::cout << "TDestructor:\t" << this << std::endl;
 	}
-	void insert(int Data, Element* Root)
+	void Destructor()
 	{
+		if (Root->pLeft)
+		{
+			Element* temp = Root->pLeft;
+			Root->pLeft = Root->pLeft->pLeft;
+			delete temp;
+		}
+		if (Root->pRight)
+		{
+			Element* temp = Root->pRight;
+			Root->pRight = Root->pRight->pRight;
+			delete temp;
+		}
+		if(Root->pLeft || Root->pRight) Destructor();
+	}
+	void insert(int Data, Element* Root = nullptr)
+	{
+		if (!Root) Root = getRoot();
 		if (this->Root == nullptr)this->Root = new Element(Data);
 		if (!Root)return;
 		if (Data < Root->Data)
@@ -48,40 +69,42 @@ public:
 			else insert(Data, Root->pRight);
 		}
 	}
-	int minValue(Element* Root)
+	int minValue(Element* Root = nullptr)
 	{
-		if (Root == nullptr)return 0;
-		//if (Root->pLeft) minValue(Root->pLeft);
-		//else return Root->Data;
+		if(!Root)Root = getRoot();
+		if (this->Root == nullptr)return 0;
 		return Root->pLeft == nullptr ? Root->Data : minValue(Root->pLeft);
 	}
-	int maxValue(Element* Root)
+	int maxValue(Element* Root = nullptr)
 	{
-		if (Root == nullptr)return 0;
-		//if (Root->pRight) maxValue(Root->pRight);
-		//else return Root->Data;
+		if (!Root)Root = getRoot();
+		if (this->Root == nullptr)return 0;
 		return Root->pRight == nullptr ? Root->Data : maxValue(Root->pRight);
 	}
-	int sum(Element* Root)
+	int sum(Element* Root = nullptr)
 	{
-		return Root == nullptr? 0 : Root->Data + sum(Root->pLeft) + sum(Root->pRight);
+		if (!Root)Root = getRoot();
+		return Root == nullptr? 0 : Root->Data + (!Root->pLeft? 0 : sum(Root->pLeft)) + (!Root->pRight ? 0 : sum(Root->pRight));
 	}
-	int Count(Element* Root)
+	int Count(Element* Root = nullptr)
 	{	
-		return Root == nullptr ? 0 : Count(Root->pLeft) + Count(Root->pRight) + 1;
+		if (!Root)Root = getRoot();
+		return Root == nullptr ? 0 : (!Root->pLeft ? 0 : Count(Root->pLeft)) + (!Root->pRight ? 0 : Count(Root->pRight)) + 1;
 	}
 	double Avg()
 	{
 		return double(sum(this->Root)) / Count(this->Root);
 	}
-	void print(Element* Root)
+	void print(Element* Root = nullptr)
 	{
+		if (!Root)Root = getRoot();
 		if (Root == nullptr)return;
-		print(Root->pLeft);
+		if(Root->pLeft) print(Root->pLeft);
 		std::cout << Root->Data << "\t";
-		print(Root->pRight);
+		if (Root->pRight)print(Root->pRight);
 	}
 	friend class UniqueTree;
+	friend class Element;
 };
 
 class UniqueTree: public Tree
@@ -112,26 +135,30 @@ void main()
 	Tree tree;
 	for (int i = 0; i < n; i++)
 	{
-		tree.insert(rand() % 100, tree.gerRoot());
+		tree.insert(rand() % 100);
 	}
-	tree.print(tree.gerRoot());
+	tree.print();
 	std::cout << std::endl;
-	std::cout << "Минимальное значение в дереве: " << tree.minValue(tree.gerRoot()) << std::endl;
-	std::cout << "Минимальное значение в дереве: " << tree.maxValue(tree.gerRoot()) << std::endl;
-	std::cout << "Сумма Элементов дерева:\t\t " << tree.sum(tree.gerRoot()) << std::endl;
-	std::cout << "Кол-во элементов дерева :\t " << tree.Count(tree.gerRoot()) << std::endl;
+	std::cout << "Минимальное значение в дереве: " << tree.minValue() << std::endl;
+	std::cout << "Максимальное значение в дереве: " << tree.maxValue() << std::endl;
+	std::cout << "Сумма Элементов дерева:\t\t " << tree.sum() << std::endl;
+	std::cout << "Кол-во элементов дерева :\t " << tree.Count() << std::endl;
 	std::cout << "Средне-арифметическое элементов дерева :\t " << tree.Avg() << std::endl;
+	std::cout << delimitr << std::endl;
+	//tree.Destructor();
+	//tree.print();
+	std::cout << delimitr << std::endl;
 
 	UniqueTree u_tree;
 	for (int i = 0; i < n; i++)
 	{
-		u_tree.insert(rand() % 100, u_tree.gerRoot());
+		u_tree.insert(rand() % 100, u_tree.getRoot());
 	}
-	u_tree.print(u_tree.gerRoot());
+	u_tree.print();
 	std::cout << std::endl;
-	std::cout << "Минимальное значение в дереве: " << u_tree.minValue(u_tree.gerRoot()) << std::endl;
-	std::cout << "Минимальное значение в дереве: " << u_tree.maxValue(u_tree.gerRoot()) << std::endl;
-	std::cout << "Сумма Элементов дерева:\t\t " << u_tree.sum(u_tree.gerRoot()) << std::endl;
-	std::cout << "Кол-во элементов дерева :\t " << u_tree.Count(u_tree.gerRoot()) << std::endl;
+	std::cout << "Минимальное значение в дереве: " << u_tree.minValue() << std::endl;
+	std::cout << "Максимальное значение в дереве: " << u_tree.maxValue() << std::endl;
+	std::cout << "Сумма Элементов дерева:\t\t " << u_tree.sum() << std::endl;
+	std::cout << "Кол-во элементов дерева :\t " << u_tree.Count() << std::endl;
 	std::cout << "Средне-арифметическое элементов дерева :\t " << u_tree.Avg() << std::endl;
 }
